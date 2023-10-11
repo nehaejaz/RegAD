f30529c68da3393c9c88fc2dea24c38340e7500f
nehaejaz <nehaejaz29@gmail.com>
2023-10-11 17:21:45 -0400

    added stn

--- models/convnext.py
+++ models/convnext.py
@@ -11,6 +11,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 from timm.models.layers import trunc_normal_, DropPath
 from timm.models.registry import register_model
+import torch.utils.model_zoo as model_zoo
+from torch.autograd import Variable
+import numpy as np
+
+N_PARAMS = {'affine': 6,
+            'translation': 2,
+            'rotation': 1,
+            'scale': 2,
+            'shear': 2,
+            'rotation_scale': 3,
+            'translation_scale': 4,
+            'rotation_translation': 3,
+            'rotation_translation_scale': 5}
 
 class Block(nn.Module):
     r""" ConvNeXt Block. There are two equivalent implementations:
@@ -48,6 +61,130 @@ class Block(nn.Module):
 
         x = input + self.drop_path(x)
         return x
+    
+def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        groups=groups,
+        bias=False,
+        dilation=dilation,
+    )
+    
+class STNModule(nn.Module):
+    def __init__(self, in_num, block_index, args):
+        super(STNModule, self).__init__()
+
+        self.feat_size = block_index
+        self.stn_mode = "rotation_scale"
+        self.stn_n_params = N_PARAMS[self.stn_mode]
+        self.in_num = in_num
+        self.conv = nn.Sequential(
+            conv3x3(in_planes=in_num, out_planes=64),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            conv3x3(in_planes=64, out_planes=16),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(in_features=16 * self.feat_size * self.feat_size, out_features=1024),
+            nn.ReLU(True),
+            nn.Linear(in_features=1024, out_features=self.stn_n_params),
+        )
+        
+        self.fc[2].weight.data.fill_(0)
+        self.fc[2].weight.data.zero_()
+
+        if self.stn_mode == 'affine':
+            self.fc[2].bias.data.copy_(torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float))
+        elif self.stn_mode in ['translation', 'shear']:
+            self.fc[2].bias.data.copy_(torch.tensor([0, 0], dtype=torch.float))
+        elif self.stn_mode == 'scale':
+            self.fc[2].bias.data.copy_(torch.tensor([1, 1], dtype=torch.float))
+        elif self.stn_mode == 'rotation':
+            self.fc[2].bias.data.copy_(torch.tensor([0], dtype=torch.float))
+        elif self.stn_mode == 'rotation_scale':
+            self.fc[2].bias.data.copy_(torch.tensor([0, 1, 1], dtype=torch.float))
+        elif self.stn_mode == 'translation_scale':
+            self.fc[2].bias.data.copy_(torch.tensor([0, 0, 1, 1], dtype=torch.float))
+        elif self.stn_mode == 'rotation_translation':
+            self.fc[2].bias.data.copy_(torch.tensor([0, 0, 0], dtype=torch.float))
+        elif self.stn_mode == 'rotation_translation_scale':
+            self.fc[2].bias.data.copy_(torch.tensor([0, 0, 0, 1, 1], dtype=torch.float))
+
+    def forward(self, x):
+        # print("feat_size",self.feat_size)
+        # print("in_num",self.in_num)
+        mode = self.stn_mode
+        batch_size = x.size(0)
+        conv_x = self.conv(x)
+        # print("conv_x shape",conv_x.shape)
+        theta = self.fc(conv_x.view(batch_size, -1))
+        # print("theta", theta.shape)
+
+        if mode == 'affine':
+            theta1 = theta.view(batch_size, 2, 3)
+        else:
+            theta1 = Variable(torch.zeros([batch_size, 2, 3], dtype=torch.float32, device=x.get_device()),
+                              requires_grad=True)
+            theta1 = theta1 + 0
+            theta1[:, 0, 0] = 1.0
+            theta1[:, 1, 1] = 1.0
+            if mode == 'translation':
+                theta1[:, 0, 2] = theta[:, 0]
+                theta1[:, 1, 2] = theta[:, 1]
+            elif mode == 'rotation':
+                angle = theta[:, 0]
+                theta1[:, 0, 0] = torch.cos(angle)
+                theta1[:, 0, 1] = -torch.sin(angle)
+                theta1[:, 1, 0] = torch.sin(angle)
+                theta1[:, 1, 1] = torch.cos(angle)
+            elif mode == 'scale':
+                theta1[:, 0, 0] = theta[:, 0]
+                theta1[:, 1, 1] = theta[:, 1]
+            elif mode == 'shear':
+                theta1[:, 0, 1] = theta[:, 0]
+                theta1[:, 1, 0] = theta[:, 1]
+            elif mode == 'rotation_scale':
+                angle = theta[:, 0]
+                theta1[:, 0, 0] = torch.cos(angle) * theta[:, 1]
+                theta1[:, 0, 1] = -torch.sin(angle)
+                theta1[:, 1, 0] = torch.sin(angle)
+                theta1[:, 1, 1] = torch.cos(angle) * theta[:, 2]
+            elif mode == 'translation_scale':
+                theta1[:, 0, 2] = theta[:, 0]
+                theta1[:, 1, 2] = theta[:, 1]
+                theta1[:, 0, 0] = theta[:, 2]
+                theta1[:, 1, 1] = theta[:, 3]
+            elif mode == 'rotation_translation':
+                angle = theta[:, 0]
+                theta1[:, 0, 0] = torch.cos(angle)
+                theta1[:, 0, 1] = -torch.sin(angle)
+                theta1[:, 1, 0] = torch.sin(angle)
+                theta1[:, 1, 1] = torch.cos(angle)
+                theta1[:, 0, 2] = theta[:, 1]
+                theta1[:, 1, 2] = theta[:, 2]
+            elif mode == 'rotation_translation_scale':
+                angle = theta[:, 0]
+                theta1[:, 0, 0] = torch.cos(angle) * theta[:, 3]
+                theta1[:, 0, 1] = -torch.sin(angle)
+                theta1[:, 1, 0] = torch.sin(angle)
+                theta1[:, 1, 1] = torch.cos(angle) * theta[:, 4]
+                theta1[:, 0, 2] = theta[:, 1]
+                theta1[:, 1, 2] = theta[:, 2]
+
+        grid = F.affine_grid(theta1, torch.Size(x.shape))
+        img_transform = F.grid_sample(x, grid, padding_mode="reflection")
+
+        return img_transform, theta1
 
 class ConvNeXt(nn.Module):
     r""" ConvNeXt
@@ -63,14 +200,15 @@ class ConvNeXt(nn.Module):
         layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
         head_init_scale (float): Init scaling value for classifier weights and biases. Default: 1.
     """
-    def __init__(self, in_chans=3, num_classes=1000, 
-                 depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], drop_path_rate=0., 
-                 layer_scale_init_value=1e-6, head_init_scale=1.,
+    def __init__(self, args, in_chans=3, num_classes=1000, 
+                 depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], stn=False, drop_path_rate=0., 
+                 layer_scale_init_value=1e-6, head_init_scale=1., 
                  ):
         super().__init__()
-
+        self.args = "rotation_scale"
         self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
         self.output_layers = []
+        self.stn = stn
         stem = nn.Sequential(
             nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
             LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
@@ -84,17 +222,37 @@ class ConvNeXt(nn.Module):
             self.downsample_layers.append(downsample_layer)
 
         self.stages = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks
+        self.stn_modules = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks
+
+        # self.stn1 = STNModule(96, 1, args)
+        # self.stn2 = STNModule(192, 2, args)
+        # self.stn3 = STNModule(384, 3, args)
+        # self.stn3 = STNModule(768, 4, args)
+
+
         dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
         cur = 0
+        block_num = [14,7,4,2]
         for i in range(4):
+            #Adding sequential layer
             stage = nn.Sequential(
                 *[Block(dim=dims[i], drop_path=dp_rates[cur + j], 
                 layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
             )
+            #Appending that into stages
             self.stages.append(stage)
-            cur += depths[i]
+            
+            if self.stn is True:
+                #Adding stn layer
+                stn = STNModule(dims[i], block_num[i] , "rotation_scale")
 
+                #Appending that into stages
+                self.stages.append(stn)
+
+            cur += depths[i]
+     
         self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
+        
         self.head = nn.Linear(dims[-1], num_classes)
 
         self.apply(self._init_weights)
@@ -104,18 +262,37 @@ class ConvNeXt(nn.Module):
     def _init_weights(self, m):
         if isinstance(m, (nn.Conv2d, nn.Linear)):
             trunc_normal_(m.weight, std=.02)
-            nn.init.constant_(m.bias, 0)
+            if(m.bias is not None): 
+                nn.init.constant_(m.bias, 0)
+                
+    def _fixstn(self, x, theta):
+        grid = F.affine_grid(theta, torch.Size(x.shape))
+        img_transform = F.grid_sample(x, grid, padding_mode="reflection")
+        return img_transform
 
     def forward_features(self, x):
         self.output_layers = []
-        for i in range(4):
-            x = self.downsample_layers[i](x)
-            # print(x.shape)
-            x = self.stages[i](x)
-            self.output_layers.append(x)
-            self.stn1 = STNModule(64, 1, args)
-            # print(x.shape)
+        ds_lay_in = 0
+        if self.stn is True:
 
+            for i in range(0,8,2):
+                x = self.downsample_layers[ds_lay_in](x)
+                x = self.stages[i](x)
+                # print("before",x.shape)
+                x, theta = self.stages[i+1](x)
+                tmp = np.tile(np.array([0, 0, 1]), (x.shape[0], 1, 1)).astype(np.float32)
+                fixtheata = torch.from_numpy(np.linalg.inv(np.concatenate((theta.detach().cpu().numpy(), tmp), axis=1))[:,:-1,:]).cuda(3)
+                x = self._fixstn(x.detach(), fixtheata)
+                # print("after",x.shape)
+                self.output_layers.append(x)
+                ds_lay_in += 1
+        else:
+            for i in range(4):
+                x = self.downsample_layers[ds_lay_in](x)
+                x = self.stages[i](x)
+                # print("before",x.shape)
+                self.output_layers.append(x)
+                ds_lay_in += 1
         # return self.norm(x.mean([-2, -1])) # global average pooling, (N, C, H, W) -> (N, C)
         return x
 
@@ -164,12 +341,20 @@ model_urls = {
 }
 
 @register_model
-def convnext_tiny(pretrained=True,in_22k=False, **kwargs):
-    model = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
+def convnext_tiny(args, pretrained=True,in_22k=False, **kwargs):
+    model = ConvNeXt(args, depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
+    # print(model)
     if pretrained:
         url = model_urls['convnext_tiny_22k'] if in_22k else model_urls['convnext_tiny_1k']
         checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
-        model.load_state_dict(checkpoint["model"])
+        # model.load_state_dict(checkpoint["model"])
+        model.load_state_dict(model_zoo.load_url(model_urls['convnext_tiny_22k']), strict=False)
+        # Get the state dictionary of the model
+        state_dict = model.state_dict()
+
+        # Print the keys
+        # print(state_dict.keys())
+        # print(model)
     return model
 
 @register_model
@@ -209,5 +394,5 @@ def convnext_xlarge(pretrained=False, in_22k=False, **kwargs):
         model.load_state_dict(checkpoint["model"])
     return model
 
-
-# print(model.stages[0])
+# model = convnext_tiny("rotation_scale")
+# print(model)
